cb4165cc484d2a0bfb804b3516faf6ae37a34f8c
Joyce Mayorga <joycem137@gmail.com>
2019-05-27 15:42:54 -0700

    Added max bio length to production sample and other settings.

--- app/models/account.rb
+++ app/models/account.rb
@@ -62,6 +62,8 @@ class Account < ApplicationRecord
   include AccountCounters
   include DomainNormalizable
 
+  MAX_NOTE_LENGTH = (ENV['MAX_BIO_LENGTH'] || 500).to_i
+
   enum protocol: [:ostatus, :activitypub]
 
   validates :username, presence: true
@@ -75,7 +77,7 @@ class Account < ApplicationRecord
   validates_with UniqueUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
-  validates :note, note_length: { maximum: 500 }, if: -> { local? && will_save_change_to_note? }
+  validates :note, note_length: { maximum: MAX_NOTE_LENGTH }, if: -> { local? && will_save_change_to_note? }
   validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
 
   scope :remote, -> { where.not(domain: nil) }
